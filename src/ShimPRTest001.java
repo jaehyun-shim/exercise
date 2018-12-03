04c520ab29cdbf432e07319d2bfe2e5b41837eee
심재현 <39612101+jaehyun-shim@users.noreply.github.com>
2018-12-03 17:54:03 +0900

    Create ShimPRTest001.java

---
+++ src/ShimPRTest001.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+/**
+ * @author Jaeshyun-shim
+ * @since 2018-12-02
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("test001");
+    }
+}
